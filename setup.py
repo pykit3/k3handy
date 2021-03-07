840c60d324a72dbe8faff6b00ffad364d80032d0
drdr xp <drdr.xp@gmail.com>
2021-03-07 23:01:57 +0800

    release: v0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="k3handy",
     packages=["k3handy"],
-    version="0.1.2",
+    version="0.1.3",
     license='MIT',
     description='handy alias of mostly used functions',
     long_description='# k3handy\n\n[![Build Status](https://travis-ci.com/pykit3/k3handy.svg?branch=master)](https://travis-ci.com/pykit3/k3handy)\n[![Documentation Status](https://readthedocs.org/projects/k3handy/badge/?version=stable)](https://k3handy.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3handy)](https://pypi.org/project/k3handy)\n\nhandy alias of mostly used functions\n\nk3handy is a component of [pykit3] project: a python3 toolkit set.\n\n\n# Install\n\n```\npip install k3handy\n```\n\n# Synopsis\n\n```python\n\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3',
