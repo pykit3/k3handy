1e6328f2fee9801adcbdf1ee0bf94947573cf510
drdr xp <drdr.xp@gmail.com>
2021-05-17 11:21:22 +0800

    release: v0.1.5

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 setuptools.setup(
     name="k3handy",
     packages=["k3handy"],
-    version="0.1.4",
+    version="0.1.5",
     license='MIT',
     description='handy alias of mostly used functions',
-    long_description='# k3handy\n\n[![Build Status](https://travis-ci.com/pykit3/k3handy.svg?branch=master)](https://travis-ci.com/pykit3/k3handy)\n[![Documentation Status](https://readthedocs.org/projects/k3handy/badge/?version=stable)](https://k3handy.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3handy)](https://pypi.org/project/k3handy)\n\nhandy alias of mostly used functions\n\nk3handy is a component of [pykit3] project: a python3 toolkit set.\n\n\n# Install\n\n```\npip install k3handy\n```\n\n# Synopsis\n\n```python\n\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3',
+    long_description='# k3handy\n\n[![Action-CI](https://github.com/pykit3/k3handy/actions/workflows/python-package.yml/badge.svg)](https://github.com/pykit3/k3handy/actions/workflows/python-package.yml)\n[![Build Status](https://travis-ci.com/pykit3/k3handy.svg?branch=master)](https://travis-ci.com/pykit3/k3handy)\n[![Documentation Status](https://readthedocs.org/projects/k3handy/badge/?version=stable)](https://k3handy.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3handy)](https://pypi.org/project/k3handy)\n\nhandy alias of mostly used functions\n\nk3handy is a component of [pykit3] project: a python3 toolkit set.\n\n\nk3handy is collection of mostly used  utilities.\n\n\n\n# Install\n\n```\npip install k3handy\n```\n\n# Synopsis\n\n```python\n\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3',
     long_description_content_type="text/markdown",
     author='Zhang Yanpo',
     author_email='drdr.xp@gmail.com',
@@ -14,7 +14,7 @@ setuptools.setup(
     keywords=['handy', 'shortcut'],
     python_requires='>=3.0',
 
-    install_requires=['semantic_version~=2.8.5', 'jinja2~=2.11.2', 'PyYAML~=5.3.1', 'sphinx~=3.3.1', 'k3ut~=0.1.7', 'k3proc~=0.2.5', 'k3str~=0.1.1'],
+    install_requires=['k3ut<0.2,>=0.1.15', 'k3proc>=0.2.13,<0.3', 'k3str<0.2,>=0.1.4'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
